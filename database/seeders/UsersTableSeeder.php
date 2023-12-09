4ff419b9ebaf59201d6b4872295b4dfa43d942e9
Andregit93 <andresevtian501@gmail.com>
2023-12-10 03:29:38 +0700

    add bounding box cloumn to report disease table

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,14 +4,28 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
-     */
+     
+Run the database seeds.*/
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $index) {
+            $userData = [
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $faker->password,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+
+            DB::table('users')->insert($userData);
+        }
     }
 }
