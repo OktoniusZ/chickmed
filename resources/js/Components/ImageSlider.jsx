d602fa6111afd41cd2f94706aa027252a0dc9ab8
jihanapriliani <11211046@student.itk.ac.id>
2023-12-21 05:51:39 +0800

    fixing about responsiveness

--- resources/js/Components/ImageSlider.jsx
+++ resources/js/Components/ImageSlider.jsx
@@ -1,7 +1,9 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+
 import "swiper/css";
-import kamil from '../../images/kamil.jpg'
-import syair from '../../images/syair.jpeg'
+import kamil from "../../images/kamil.jpg";
+import syair from "../../images/syair.jpeg";
+import jihan from "../../images/jihan.png";
 
 export default function ImageSlider() {
     let teams = [
@@ -13,12 +15,12 @@ export default function ImageSlider() {
         {
             nama: "Syair Dafiq Faizur Rahman",
             job: "Machine Learning",
-            url: "../../images/kamil.jpg",
+            url: syair,
         },
         {
             nama: "Jihan Apriliani Nurhasanah",
             job: "Machine Learning",
-            url: "https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            url: jihan,
         },
         {
             nama: "Muhammad Insan Kamil",
@@ -28,28 +30,46 @@ export default function ImageSlider() {
         {
             nama: "Oktonius Zevanya Simanungkalit",
             job: "Cloud Computing",
-            url: "../../images/kamil.jpg",
+            url: syair,
         },
         {
             nama: "Andre Sevtian",
             job: "Cloud Computing",
-            url: "../../images/kamil.jpg",
+            url: syair,
         },
     ];
     return (
         <>
             {/* Start Team Page */}
-            <div className="pb-48 px-48">
+            <div className="max-w-screen-2xl mx-auto my-32 cursor-pointer">
                 <h1 className="text-center font-bold text-3xl mb-8">
                     Slide To <span className="text-orange-600">Know</span> Us
                     Better
                 </h1>
 
                 <Swiper
-                    spaceBetween={20}
-                    slidesPerView={4}
+                    spaceBetween={5}
                     onSlideChange={() => console.log("slide change")}
                     onSwiper={(swiper) => console.log(swiper)}
+                    grabCursor={true}
+                    breakpoints={{
+                        // Mobile (less than 640px)
+                        640: {
+                            slidesPerView: 1,
+                        },
+                        // Tablet (less than 768px)
+                        768: {
+                            slidesPerView: 2,
+                        },
+                        // Desktop (less than 1024px)
+                        1024: {
+                            slidesPerView: 3,
+                        },
+                        // Large Desktop (1024px and above)
+                        1200: {
+                            slidesPerView: 4,
+                        },
+                    }}
                 >
                     {teams.map((data, i) => {
                         return (
@@ -58,18 +78,18 @@ export default function ImageSlider() {
                                     className="flex justify-center items-center"
                                     key={i}
                                 >
-                                    <div className="relativ">
+                                    <div className="relative">
                                         <img
                                             className="object-cover w-80 h-80"
                                             src={data.url}
                                         />
                                         <div className="mr-94 flex justify-center">
-                                            <div className="absolute w-3/4  h-16 bg-white bottom-5">
+                                            <div className="absolute w-3/4 p-3 bg-white bottom-5">
                                                 <div className="mt-2 ml-4">
                                                     <h1 className="font-bold">
                                                         {data.nama}
                                                     </h1>
-                                                    <p className="text-gray-600 text-sm">
+                                                    <p className="text-orange-600 text-sm">
                                                         {data.job}
                                                     </p>
                                                 </div>
