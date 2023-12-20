92dd8e135c4a591e18a149c27951fd889b0b969f
jihanapriliani <11211046@student.itk.ac.id>
2023-12-21 07:38:57 +0800

    fixing images in home and about

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -3,6 +3,9 @@ import Footer from "@/Components/Footer";
 import Navbar from "@/Components/Navbar";
 import Article from "@/Components/Article";
 
+import HomeHero from "../../images/hero-home.png";
+import HeroSolution from "../../images/hero-solution.png";
+
 export default function Home() {
     return (
         <>
@@ -11,7 +14,7 @@ export default function Home() {
 
             {/* Start Landing Page */}
             <div className="max-w-screen-2xl flex flex-col my-24 mt-6 lg:my-24 lg:mb-48 p-6 text-center lg:text-left lg:flex-row justify-between items-center mx-auto">
-                <div className="flex flex-col justify-center items-center lg:items-start  mb-8 lg:mb-0">
+                <div className="flex flex-col justify-center items-center lg:items-start  mb-8 lg:mb-0 lg:w-1/2">
                     <h1 className="font-bold text-5xl md:text-6xl mb-8">
                         <span className="text-orange-600">ChickMed</span> -
                         Poultry <br className="hidden md:block" /> Health
@@ -26,39 +29,39 @@ export default function Home() {
                         Get Started
                     </button>
                 </div>
-                <div>
+                <div className="lg:w-1/2">
                     <img
-                        src="https://images.unsplash.com/photo-1556316918-880f9e893822?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        src={HomeHero}
                         alt="Chicken"
-                        className="h-80 w-120 object-cover"
+                        className="object-cover"
                     />
                 </div>
             </div>
             {/* End Landing Page */}
 
             {/* Start Solusi Page */}
-            <div className="flex justify-center flex-col lg:flex-row lg:justify-between items-center p-6 text-center lg:text-left my-48 max-w-screen-2xl mx-auto">
-                <div className="">
+            <div className="flex justify-center flex-col lg:flex-row-reverse lg:justify-between items-center p-6 text-center lg:text-right my-48 max-w-screen-2xl mx-auto">
+                <div className="lg:w-1/2">
                     <h1 className="font-bold text-4xl md:text-6xl mb-8 max-w-xl">
                         Solusi Pintar Untuk{" "}
                         <span className="text-orange-600">Kesehatan</span> Ayam{" "}
                         <br /> Anda
                     </h1>
-                    <p className="mb-6 lg:mb-8 max-w-md mx-auto lg:mx-0">
+                    <p className="mb-6 lg:mb-8 max-w-xl mx-auto lg:mx-0">
                         Dengan ChickMed, Anda memiliki alat yang andal untuk
                         merawat ayam peliharaan dengan lebih baik.
                     </p>
-                    <p className="mb-6 lg:mb-8 max-w-md mx-auto lg:mx-0">
+                    <p className="mb-6 lg:mb-8 max-w-xl mx-auto lg:mx-0">
                         Kami memadukan kecerdasan buatan dengan gambar kotoran
                         ayam untuk mendeteksi penyakit dengan cepat dan akurat.
                         Lindungi kawanan ayam Anda dengan aplikasi canggih ini!
                     </p>
                 </div>
-                <div>
+                <div className="lg:w-1/2 lg:ml-[-100px]">
                     <img
-                        src="https://images.unsplash.com/photo-1546272989-40c92939c6c2?q=80&w=1882&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        src={HeroSolution}
                         alt="Chick"
-                        className="object-cover h-96"
+                        className="object-cover"
                     />
                 </div>
             </div>
@@ -75,89 +78,14 @@ export default function Home() {
                         ayam yang bisa bebas dibaca oleh semua pengguna.
                     </p>
                 </div>
-                <div className="flex justify-center items-center flex-col md:flex-row flex-wrap">
-                    <div className="shadow-sm px-6 py-8 cursor-pointer">
-                        <img
-                            src="https://images.unsplash.com/photo-1546272989-40c92939c6c2?q=80&w=1882&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            className="object-cover h-72"
-                            alt=""
-                        />
-                        <div className="flex my-1">
-                            {/* <p>
-                                <FontAwesomeIcon
-                                    icon={faBook}
-                                    className="text-orange-600 mr-3"
-                                />
-                            </p> */}
-                            <p className="text-xs text-gray-600 leading-6">
-                                Diupload 3 minggu yang lalu..
-                            </p>
-                        </div>
-                        <div>
-                            <h1 className="font-bold">
-                                Identifikasi kesehatan ayam
-                            </h1>
-                            <p className="w-72 text-gray-600 text-sm">
-                                Ayam sehat memiliki jengger berwarna merah cerah
-                                dan tekstur yang...
-                            </p>
-                        </div>
-                    </div>
-                    <div className="shadow-sm px-6 py-8 cursor-pointer">
-                        <img
-                            src="https://images.unsplash.com/photo-1546272989-40c92939c6c2?q=80&w=1882&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            className="object-cover h-72"
-                            alt=""
-                        />
-                        <div className="flex my-1">
-                            {/* <p>
-                                <FontAwesomeIcon
-                                    icon={faBook}
-                                    className="text-orange-600 mr-3"
-                                />
-                            </p> */}
-                            <p className="text-xs text-gray-600 leading-6">
-                                Diupload 3 minggu yang lalu..
-                            </p>
-                        </div>
-                        <div>
-                            <h1 className="font-bold">
-                                Identifikasi kesehatan ayam
-                            </h1>
-                            <p className="w-72 text-gray-600 text-sm">
-                                Ayam sehat memiliki jengger berwarna merah cerah
-                                dan tekstur yang...
-                            </p>
-                        </div>
-                    </div>
-                    <div className="shadow-sm px-6 py-8 cursor-pointer">
-                        <img
-                            src="https://images.unsplash.com/photo-1546272989-40c92939c6c2?q=80&w=1882&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            className="object-cover h-72"
-                            alt=""
-                        />
-                        <div className="flex my-1">
-                            {/* <p>
-                                <FontAwesomeIcon
-                                    icon={faBook}
-                                    className="text-orange-600 mr-3"
-                                />
-                            </p> */}
-                            <p className="text-xs text-gray-600 leading-6">
-                                Diupload 3 minggu yang lalu..
-                            </p>
-                        </div>
-                        <div>
-                            <h1 className="font-bold">
-                                Identifikasi kesehatan ayam
-                            </h1>
-                            <p className="w-72 text-gray-600 text-sm">
-                                Ayam sehat memiliki jengger berwarna merah cerah
-                                dan tekstur yang...
-                            </p>
-                        </div>
-                    </div>
+
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 mb-10">
+                    <Article />
+                    <Article />
+                    <Article />
+                    <Article />
                 </div>
+
                 <p className="text-center text-orange-600 underline font-bold mt-12">
                     <a href="#">See more article</a>
                 </p>
